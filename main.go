b18edaae97df8c3819431a430afb1728610fd140
Douglas Lamb <douglaslamb@douglaslamb.com>
2016-04-16 01:27:02 -0500

    got go reading the csv. still need to get it all into a struct

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
-  "fmt"
-  "html"
-  "log"
-  "net/http"
+	//"encoding/csv"
+	"fmt"
+	"html"
+	//"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 )
 
+type Business struct {
+	Id         uint32
+	Uuid       string
+	Name       string
+	Address    string
+	Address2   string
+	City       string
+	State      string
+	Zip        uint16
+	Country    string
+	Phone      uint32
+	Website    string
+	Created_at time.Time
+}
+
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-  })
+	dat, err := ioutil.ReadFile("resources/engineering_project_businesses.csv")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dat))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
 
-  log.Fatal(http.ListenAndServe(":8080", nil))
-
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
