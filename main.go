f8417432bbe560cf9079b935708266978d6d7312
Douglas Lamb <douglaslamb@douglaslamb.com>
2016-04-18 21:49:29 -0500

    got csv ingested into an array of Business structs

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	//"encoding/csv"
+	"encoding/csv"
 	"fmt"
 	"html"
-	//"io"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -27,14 +29,50 @@ type Business struct {
 }
 
 func main() {
+	// read file
 	dat, err := ioutil.ReadFile("resources/engineering_project_businesses.csv")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(dat))
+	inString := string(dat)
+	businessList := csv.NewReader(strings.NewReader(inString))
+	businessSlice := make([]Business, 0, 10)
+
+	fmt.Println(businessList.Read())
+	for {
+		record, err := businessList.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		business := new(Business)
+		id, _ := strconv.Atoi(record[0])
+		business.Id = uint32(id)
+		business.Uuid = record[1]
+		business.Name = record[2]
+		business.Address = record[3]
+		business.Address2 = record[4]
+		business.City = record[5]
+		business.State = record[6]
+		zip, _ := strconv.Atoi(record[7])
+		business.Zip = uint16(zip)
+		business.Country = record[8]
+		phone, _ := strconv.Atoi(record[9])
+		business.Phone = uint32(phone)
+		business.Website = record[10]
+		createdAt, _ := time.Parse("2006-01-02 15:04:05", record[11])
+		business.Created_at = createdAt
+		businessSlice = append(businessSlice, *business)
+	}
+	fmt.Println(businessSlice[10])
+
+	//create request handling
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	//log.Fatal(http.ListenAndServe(":8080", nil))
 }
